63c352a28ea5c7d72ac830351dece28d9df96496
ogawa-takuma <tkm.op119@gmail.com>
2018-08-11 21:44:52 +0900

    ファイルをtabelog_infoの中に作成する

--- main.go
+++ main.go
@@ -2,8 +2,20 @@ package main
 
 import(
 	"fmt"
+	"os"
 )
 
+var fileName = "TabelogInfo.json"
+
+func makeJSONFile() {
+	file, err := os.Create("./tabelog_info/" + fileName)
+	if err != nil {
+		fmt.Println("ファイルが作成できませんでした。")
+	}else {
+		fmt.Println("ファイルを作成しました。\n%v\n", file)
+	}
+}
+
 func main(){
-	fmt.Printf("Hellow, World!\n")
+	makeJSONFile()
 }
